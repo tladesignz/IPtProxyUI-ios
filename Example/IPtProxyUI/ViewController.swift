e98574f0eb2113443efd9b48987196cd89fb9c3b
Benjamin Erhart <berhart@netzarchitekten.com>
2023-03-10 16:34:06 +0100

    Added example project for easier development.

---
+++ Example/IPtProxyUI/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  IPtProxyUI
+//
+//  Created by Benjamin Erhart on 03/10/2023.
+//  Copyright (c) 2023 Benjamin Erhart. All rights reserved.
+//
+
+import UIKit
+import IPtProxyUI
+
+class ViewController: UIViewController, BridgesConfDelegate {
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        bridgeSettings()
+    }
+
+
+    // MARK: BridgesConfDelegate
+
+    var transport = Settings.transport
+
+    var customBridges = Settings.customBridges
+
+    var saveButtonTitle: String?
+
+    func save() {
+        Settings.transport = transport
+        Settings.customBridges = customBridges
+    }
+
+
+    // MARK: Actions
+
+    @IBAction func bridgeSettings() {
+        let vc = BridgesConfViewController()
+        vc.delegate = self
+        vc.transport = transport
+        vc.customBridges = customBridges
+
+        present(UINavigationController(rootViewController: vc), animated: true)
+    }
+}
+
---
+++ Example/IPtProxyUI_Mac/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  IPtProxyUI_Mac_Example
+//
+//  Created by Benjamin Erhart on 10.03.23.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import Cocoa
+import IPtProxyUI
+
+class ViewController: NSViewController, NSWindowDelegate, BridgesConfDelegate {
+
+    override func viewDidAppear() {
+        super.viewDidAppear()
+
+        bridgeSettings(self)
+    }
+
+    // MARK: NSWindowDelegate
+
+    public func windowWillClose(_ notification: Notification) {
+        NSApp.stopModal()
+    }
+
+
+    // MARK: BridgesConfDelegate
+
+    var transport = Settings.transport
+
+    var customBridges: [String]? = Settings.customBridges
+
+    var saveButtonTitle: String? = nil
+
+    func save() {
+        Settings.transport = transport
+        Settings.customBridges = customBridges
+    }
+
+
+    // MARK: Actions
+
+    @IBAction func bridgeSettings(_ sender: Any) {
+        let vc = BridgesConfViewController()
+        vc.delegate = self
+        vc.transport = transport
+        vc.customBridges = customBridges
+
+        let window = NSWindow(contentViewController: vc)
+        window.delegate = self
+
+        NSApp.runModal(for: window)
+
+        window.close()
+    }
+}
+
