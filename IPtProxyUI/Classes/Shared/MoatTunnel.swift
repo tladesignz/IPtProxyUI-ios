3cfd6e29abd9101e7d73c56ec66f1e4d5f1cca4b
Benjamin Erhart <berhart@netzarchitekten.com>
2026-01-23 12:50:39 +0100

    Added support for a DNSTT Moat/RdSys tunnel.

--- IPtProxyUI/Classes/Shared/MoatTunnel.swift
+++ IPtProxyUI/Classes/Shared/MoatTunnel.swift
@@ -24,41 +24,53 @@ extension String {
 }
 
 public enum MoatTunnel: String {
-    case meek
+	case meek
+	case dnstt
 
-    static let meekConf = "url=https://1723079976.rsc.cdn77.org;front=www.phpmyadmin.net"
+	static let meekConf = "url=https://1723079976.rsc.cdn77.org;front=www.phpmyadmin.net"
+	private static let dnsttConf = ""
 
-    var transport: Transport {
-        switch self {
-        case .meek:
-            return .meek
-        }
-    }
+	var transport: Transport {
+		switch self {
+		case .meek:
+			return .meek
 
-    var baseUrl: URL? {
-        switch self {
-        case .meek:
-            return URL(string: "https://bridges.torproject.org")
-        }
-    }
+		case .dnstt:
+			return .dnstt
+		}
+	}
 
-    var proxyConf: [AnyHashable: Any] {
-        let user: String
-        let password: String
+	var baseUrl: URL? {
+		switch self {
+		case .meek:
+			return URL(string: "https://bridges.torproject.org")
 
-        switch self {
-        case .meek:
-            user = Self.meekConf.firstHalf
-            password = Self.meekConf.secondHalf
-        }
+		case .dnstt:
+			return URL(string: "")
+		}
+	}
 
-        return [
-            kCFProxyTypeKey as AnyHashable: kCFProxyTypeSOCKS,
-            kCFStreamPropertySOCKSVersion: kCFStreamSocketSOCKSVersion5,
-            kCFStreamPropertySOCKSProxyHost: "127.0.0.1",
-            kCFStreamPropertySOCKSProxyPort: transport.port,
-            kCFStreamPropertySOCKSUser: user,
-            kCFStreamPropertySOCKSPassword: password,
-        ]
-    }
+	var proxyConf: [AnyHashable: Any] {
+		let user: String
+		let password: String
+
+		switch self {
+		case .meek:
+			user = Self.meekConf.firstHalf
+			password = Self.meekConf.secondHalf
+
+		case .dnstt:
+			user = Self.dnsttConf.firstHalf
+			password = Self.dnsttConf.secondHalf
+		}
+
+		return [
+			kCFProxyTypeKey as AnyHashable: kCFProxyTypeSOCKS,
+			kCFStreamPropertySOCKSVersion: kCFStreamSocketSOCKSVersion5,
+			kCFStreamPropertySOCKSProxyHost: "127.0.0.1",
+			kCFStreamPropertySOCKSProxyPort: transport.port,
+			kCFStreamPropertySOCKSUser: user,
+			kCFStreamPropertySOCKSPassword: password,
+		]
+	}
 }
